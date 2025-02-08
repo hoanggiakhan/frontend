622110b07bed0670d51578bc8c2132010bd6deb9
Hoang Gia Khanh <khanhhoanggia513@gmail.com>
2025-02-08 16:48:22 +0700

    change link

--- src/utils/UrlBackend.ts
+++ src/utils/UrlBackend.ts
@@ -1,2 +1,3 @@
-export const URL ="https://backend-txrd.onrender.com"
+export const URL = "https://backend-three-alpha-34.vercel.app"
+//"https://backend-txrd.onrender.com"
 // "http://localhost:8080"
